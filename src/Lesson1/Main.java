a75ea2dad76223c8126f456597502b62df04f024
AamirNaseer1 <amirdk46@gmail.com>
2023-09-22 17:24:52 +0500

    Committing files from Intellij IDE

--- src/Lesson1/Main.java
+++ src/Lesson1/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println(" First Program! ");
+        System.out.println(" First Java Program! ");
 
     }
 
